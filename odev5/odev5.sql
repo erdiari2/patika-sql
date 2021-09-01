76e19e0023f66713ddc5faa5306d7ba4c98b891f
erdiari <merdiari@gmail.com>
2021-09-01 16:11:16 +0300

    Done odev5

---
+++ odev5/odev5.sql
@@ -0,0 +1,3 @@
+SELECT title FROM public.film ORDER BY length ASC LIMIT 5;
+SELECT title FROM public.film ORDER BY length DESC LIMIT 5;
+SELECT last_name FROM public.customer WHERE store_id = 1 ORDER BY last_name DESC LIMIT 4;
